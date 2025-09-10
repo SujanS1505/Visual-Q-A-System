00d5ed37532ed1aec57f8d5d2ab02fe4866e26d2
Sujans1505@gmail.com <sujans1505@gmail.com>
2025-09-10 09:53:46 +0530

    tested a picture to ensure it works perfectly in testing model

--- test_model.py
+++ test_model.py
@@ -6,10 +6,10 @@ processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 
 # Load a sample image (put noodles.jpg in the same folder)
-image = Image.open("noodles.jpg")
+image = Image.open("E:\\new projects\\Visual QA System\\1073577.jpg")
 
 # Ask a question
-question = "What is the person doing?"
+question = "What is this?"
 
 # Preprocess inputs
 inputs = processor(image, question, return_tensors="pt")
