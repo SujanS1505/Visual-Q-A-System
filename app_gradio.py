19d139d32642485d0b4a5a686e5b701fca8ad5fe
Sujans1505@gmail.com <sujans1505@gmail.com>
2025-10-25 10:57:18 +0530

    changes made..

--- app_gradio.py
+++ app_gradio.py
@@ -1,6 +1,13 @@
 import gradio as gr
 from transformers import ViltProcessor, ViltForQuestionAnswering
 from PIL import Image
+import ssl
+import os
+
+# Disable SSL verification (use cautiously)
+os.environ['CURL_CA_BUNDLE'] = ''
+os.environ['REQUESTS_CA_BUNDLE'] = ''
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Load model
 processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
